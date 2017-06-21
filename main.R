432c7718550bab78434315b4502a7f60dba1ea02
Daniel Kneipp <daniel.kneipp@outlook.com>
2017-06-21 03:20:13 -0300

    First working version. Tested on Lotka-Volterra

--- main.R
+++ main.R
@@ -13,7 +13,7 @@ is_bimolecular <- function(react_str) {
     return(grepl('\\+', first_part))
 }
 
-get_stoichiometry <- function(species, reaction_part) {
+get_species_count <- function(species, reaction_part) {
     m <- gregexpr(paste('[1-9]*', species, sep = ''), reaction_part)
     matches <- regmatches(reaction_part, m)
     nums <- array(0, length(matches[[1]]))
@@ -28,63 +28,75 @@ get_stoichiometry <- function(species, reaction_part) {
     return(nums)
 }
 
-get_species_flow<- function(species, reactions) {
-    flow <- c()
-    for(i in 1:length(reactions)) {
-        f_p <- get_first_part(reactions[i])
-        s_p <- get_second_part(reactions[i])
-        
-        f_p_n <- get_stoichiometry(species, f_p)
-        s_p_n <- get_stoichiometry(species, s_p)
-        
-        r <- list(react = i, left_sto = f_p_n, right_sto = s_p_n)
-        flow <- c(flow, list(r))
+get_stoichiometry <- function(species, reaction) {
+    f_p <- get_first_part(reaction)
+    s_p <- get_second_part(reaction)
+    
+    f_p_n <- get_species_count(species, f_p)
+    s_p_n <- get_species_count(species, s_p)
+    
+    r <- list(left_sto = sum(f_p_n), right_sto = sum(s_p_n))
+    return(r)
+}
+
+reactants_in_reaction <- function(species, reaction) {
+    f_p <- get_first_part(reaction)
+    words <- strsplit(f_p, '[^a-zA-Z]')[[1]]
+    r <- c()
+    for(i in 1:length(species)) {
+        if(any(words == species[i])) {
+            r <- c(r, i)
+        }
     }
-    return(flow)
+    return(r)
 }
 
 # Reaction must be unimolecular or bimolecular
 #  E.g.: A + B -> C
 #        2A -> B
-react <- function(species, ci, reactions, qi, qmax, cmax) {
-    n_reactions <- length(reactions)
+react <- function(species, ci, reactions, ki, t) {
+    products <- matrix(data = 0, nrow = length(reactions), ncol = length(species))
+    reactants <- matrix(data = 0, nrow = length(reactions), ncol = length(species))
     
-    # Get reaction type, number of auxuliary reactions and Js 
-    # (for the diferential equations computation)
-    n_aux_spec <- 0
-    n_js <- 0
-    reaction_type <- numeric(n_reactions)
-    for(i in 1:n_reactions) {
-        if(is_bimolecular(reactions[i])) {
-            reaction_type[i] = 2
-            n_aux_spec <- n_aux_spec + 5
-            n_js <- n_js + 3
-        } else {
-            reaction_type[i] = 1
-            n_aux_spec <- n_aux_spec + 3
-            n_js <- n_js + 2
+    for(i in 1:length(reactions)) {
+        for(j in 1:length(species)) {
+            stoc <- get_stoichiometry(species[j], reactions[i])
+            products[i,j] <- stoc$right_sto
+            reactants[i,j] <- stoc$left_sto
         }
     }
     
-    y <- numeric(length(species) + n_aux_spec)
-    Js <- numeric(n_js)
+    M <- products - reactants
+    Mt <- t(M)
     
-    y_js_map <- array(list(), length(y))
-    
-    for(i in 1:length(species)) {
-        flow <- get_species_flow(species[i], reactions)
-        y_js_map[i] <- flow
+    fx <- function(t, y, parms) {
+        dy <- numeric(length(y))
+        
+        v <- matrix(data = 0, nrow = length(reactions), ncol = 1)
+        for(i in 1:length(reactions)) {
+            s_is_reac <- reactants_in_reaction(species, reactions[i])
+            v[i,1] <- ki[i] * prod(y[s_is_reac])
+        }
+        
+        dy <- Mt %*% v
+        
+        return(list(dy))
     }
     
-    for(i in length(species):length(y)) {
-    }
+    result <- ode(times = t, y = ci, func = fx, parms = NULL)
+    return(result)
 }
 
 behaviour <- react(
-    species   = c('A','B','C'),
-    ci        = c(1e3, 1e3, 0), 
-    reactions = c('A + B -> C'),
-    qi        = c(1e-7),
-    qmax      = c(1e-3),
-    cmax      = c(1e5)
+    species   = c('A', 'B'),
+    ci        = c(20, 13), 
+    reactions = c('A + B -> 2B',
+                  'A -> 2A',
+                  'B -> 0'),
+    ki        = c(1e-4,
+                  5e-3,
+                  5e-3),
+    t         = seq(0, 12000, 1)
 )
+
+matplot(x = behaviour[,1], y = behaviour[,2:dim(behaviour)[2]])
