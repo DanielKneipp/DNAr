ae2ebb4451078fba727d0f6b92005ad000530b75
Daniel Kneipp <daniel.kneipp@outlook.com>
2017-08-13 22:22:35 -0300

    Adding CRN to DSD script functionality

--- R/4domain_reactor.R
+++ R/4domain_reactor.R
@@ -336,3 +336,403 @@ react_4domain <- function(
     # initial concentrations, reactions and rate constants.
     return(result)
 }
+
+# TODO: Doc this function
+get_dsd_header_str <- function(time, species_to_plot) {
+    # Set template string
+    s <- 'directive duration %t points 1000
+directive simulation deterministicstiff
+directive concentration M
+directive compilation infinite'
+
+    # Set time
+    stringr::str_replace(s, '%t', as.character(max(time)))
+
+    # Add plot def template
+    paste(s, 'directive plot', sep = '\n')
+
+    # Add the species names to plot
+    for(i in 1:length(species_to_plot)) {
+        spec <- paste(species_to_plot[[i]], '()', sep = '')
+        paste(s, spec, sep = ' ')
+
+        # Check if it is the last species to plot
+        if(i != length(species_to_plot)) {
+            # If it isn't, add a ';'
+            paste(s, ';', sep = '')
+        }
+    }
+
+    return(s)
+}
+
+# TODO: Doc this function
+get_dsd_species_str <- function(species_name, domains) {
+    # Set the string template
+    s <- 'def %name() = (Signal(%domains))'
+
+    # Set the species name
+    stringr::str_replace(s, '%name', species_name)
+
+    # Construct the domains string
+    domains_str <- paste(domains, collapse = ', ')
+
+    # Set the domains string
+    stringr::str_replace(s, '%domains', domains_str)
+
+    return(s)
+}
+
+# TODO: Doc this function
+get_dsd_4domain_modules_str <- function() {
+    s <- '(* Input and output *)
+def Signal(unk, i1, i2, i3) = <unk i1^ i2 i3^>
+
+(* Unimolecular to one product reaction *)
+def G_1(ia, ib, ic, unko1, o1a) = {ia^*}[ib ic^]<unko1 o1a^>
+def T_1(ic, unko, oa, ob, oc) = {ic^*}[unko oa^]<ob oc^>
+
+(* Intermediate states *)
+def Waste1_uni(unki, ia, ib, ic) = <unki>[ia^ ib ic^]
+def Waste2_1(ib, ic, unko, oa) = <ib>[ic^ unko oa^]
+def P_1(ib, ic, unko, oa) = <ib ic^ unko oa^>
+
+def A_e_B(
+    qi, qmax, CiA, CiB, Cmax,
+    unki, ia, ib, ic,
+    unko, oa, ob, oc
+) = (
+      CiA  * Signal(unki, ia, ib, ic)
+    | CiB  * Signal(unko, oa, ob, oc)
+    | Cmax * G_1(ia, ib, ic, unko, oa)
+    | Cmax * T_1(ic, unko, oa, ob, oc)
+    | rxn Signal(unki, ia, ib, ic) + G_1(ia, ib, ic, unko, oa) ->{qi} Waste1_uni(unki, ia, ib, ic) + P_1(ib, ic, unko, oa)
+    | rxn P_1(ib, ic, unko, oa) + T_1(ic, unko, oa, ob, oc) ->{qmax} Waste2_1(ib, ic, unko, oa) + Signal(unko, oa, ob, oc)
+)
+
+(* Unimolecular to two products reaction *)
+def G_2(ia, ib, ic, unko1, o1a, unko2, o2a) = {ia^*}[ib ic^]<unko1 o1a^ unko2 o2a^>
+def T_2(ic, unko1, o1a, o1b, o1c, unko2, o2a, o2b, o2c) = {ic^*}[unko1 o1a^]<o1b o1c^>:[unko2 o2a^]<o2b o2c^>
+
+(* Intermediate states *)
+def Waste2_2(ib, ic, unko1, o1a, unko2, o2a) = <ib>[ic^ unko1 o1a^ unko2 o2a^]
+def P_2(ib, ic, unko1, o1a, unko2, o2a) = <ib ic^ unko1 o1a^ unko2 o2a^>
+
+def A_e_BpC(
+    qi, qmax, CiA, CiB, CiC, Cmax,
+    unki, ia, ib, ic,
+    unko1, o1a, o1b, o1c,
+    unko2, o2a, o2b, o2c
+) = (
+      CiA  * Signal(unki, ia, ib, ic)
+    | CiB  * Signal(unko1, o1a, o1b, o1c)
+    | CiC  * Signal(unko2, o2a, o2b, o2c)
+    | Cmax * G_2(ia, ib, ic, unko1, o1a, unko2, o2a)
+    | Cmax * T_2(ic, unko1, o1a, o1b, o1c, unko2, o2a, o2b, o2c)
+    | rxn Signal(unki, ia, ib, ic) + G_2(ia, ib, ic, unko1, o1a, unko2, o2a) ->{qi} Waste1_uni(unki, ia, ib, ic) + P_2(ib, ic, unko1, o1a, unko2, o2a)
+    | rxn P_2(ib, ic, unko1, o1a, unko2, o2a) + T_2(ic, unko1, o1a, o1b, o1c, unko2, o2a, o2b, o2c) ->{qmax} Waste2_2(ib, ic, unko1, o1a, unko2, o2a) + Signal(unko1, o1a, o1b, o1c) + Signal(unko2, o2a, o2b, o2c)
+)
+
+(* Bimolecular to one product reaction *)
+def L_1(i1a, i1b, i1c, i2a, i2b, i2c, unko, oa) = {i1a^*}[i1b i1c^ i2a^]:[i2b i2c^]<unko oa^>
+def B(i1b, i1c, i2a) = <i1b i1c^ i2a^>
+
+(* Intermediate states *)
+def H_1(unki1, i1a, i1b, i1c, i2a, i2b, i2c, unko, oa) = <unki1>[i1a^ i1b i1c^]:{i2a^*}[i2b i2c^]<unko oa^>
+def Waste1_bi(unki1, unki2, i1a, i1b, i1c, i2a, i2b, i2c) = <unki1>[i1a^ i1b i1c^]:<unki2>[i2a^ i2b i2c^]
+
+def ApB_e_C(
+    qi, qmax, CiA, CiB, CiC, Cmax,
+    unka, i1a, i1b, i1c,
+    unkb, i2a, i2b, i2c,
+    unkc, c1, c2, c3
+) = (
+      CiA  * Signal(unka, i1a, i1b, i1c)
+    | CiB  * Signal(unkb, i2a, i2b, i2c)
+    | CiC  * Signal(unkc, c1, c2, c3)
+    | Cmax * L_1(i1a, i1b, i1c, i2a, i2b, i2c, unkc, c1)
+    | Cmax * B(i1b, i1c, i2a)
+    | Cmax * T_1(i2c, unkc, c1, c2 ,c3)
+    | rxn Signal(unka, i1a, i1b, i1c) + L_1(i1a, i1b, i1c, i2a, i2b, i2c, unkc, c1) <->{qi}{qmax} H_1(unka, i1a, i1b, i1c, i2a, i2b, i2c, unkc, c1) + B(i1b, i1c, i2a)
+    | rxn Signal(unkb, i2a, i2b, i2c) + H_1(unka, i1a, i1b, i1c, i2a, i2b, i2c, unkc, c1) ->{qmax} Waste1_bi(unka, unkb, i1a, i1b, i1c, i2a, i2b, i2c) + P_1(ib, ic, unkc, c1)
+    | rxn P_1(ib, ic, unkc, c1) + T_1(i2c, unkc, c1, c2 ,c3) ->{qmax} Waste2_1(i2b, i2c, unkc, c1) + Signal(unkc, c1, c2, c3)
+)
+
+(* Bimolecular to two products reaction *)
+def L_2(i1a, i1b, i1c, i2a, i2b, i2c, unko1, o1a, unko2, o2a) = {i1a^*}[i1b i1c^ i2a^]:[i2b i2c^]<unko1 o1a^ unko2 o2a^>
+
+(* Intermediate states *)
+def H_2(unki1, i1a, i1b, i1c, i2a, i2b, i2c, unko1, o1a, unko2, o2a) = <unki1>[i1a^ i1b i1c^]:{i2a^*}[i2b i2c^]<unko1 o1a^ unko2 o2a^>
+
+def ApB_e_CpD(
+    qi, qmax, CiA, CiB, CiC, CiD, Cmax,
+    unka, i1a, i1b, i1c,
+    unkb, i2a, i2b, i2c,
+    unkc, o1a, o1b, o1c,
+    unkd, o2a, o2b, o2c
+) = (
+      CiA  * Signal(unka, i1a, i1b, i1c)
+    | CiB  * Signal(unkb, i2a, i2b, i2c)
+    | CiC  * Signal(unkc, o1a, o1b, o1c)
+    | CiD  * Signal(unkd, o2a, o2b, o2c)
+    | Cmax * L_2(i1a, i1b, i1c, i2a, i2b, i2c, unkc, o1a, unkd, o2a)
+    | Cmax * B(i1b, i1c, i2a)
+    | Cmax * T_2(i2c, unkc, o1a, o1b, o1c, unkd, o2a, o2b, o2c)
+    | rxn Signal(unka, i1a, i1b, i1c) + L_2(i1a, i1b, i1c, i2a, i2b, i2c, unkc, o1a, unkd, o2a) <->{qi}{qmax} H_2(unka, i1a, i1b, i1c, i2a, i2b, i2c, unkc, o1a, unkd, o2a) + B(i1b, i1c, i2a)
+    | rxn Signal(unkb, i2a, i2b, i2c) + H_2(unka, i1a, i1b, i1c, i2a, i2b, i2c, unkc, o1a, unkd, o2a) ->{qmax} Waste1_bi(unka, unkb, i1a, i1b, i1c, i2a, i2b, i2c) + P_2(i2b, i2c, unkc, o1a, unkd, o2a)
+    | rxn P_2(i2b, i2c, unkc, o1a, unkd, o2a) + T_2(i2c, unkc, o1a, o1b, o1c, unkd, o2a, o2b, o2c) ->{qmax} Waste2_2(i2b, i2c, unkc, o1a, unkd, o2a) + Signal(unkc, o1a, o1b, o1c) + Signal(unkd, o2a, o2b, o2c)
+)
+
+(* Buffer module *)
+def LS(ia, ib, ic, d) = {ia^*}[ib ic^ d^]
+def BS(ib, ic, d) = <ib ic^ d^>
+
+(* Intermediate states *)
+def HS(unki, ia, ib, ic, d) = <unki>[ia^ ib ic^]{d^*}
+
+def Buff(
+    qs, qmax, Cmax, Cii, d,
+    unki, ia, ib, ic
+) = (
+      Cii * Signal(unki, ia, ib, ic)
+    | Cmax * LS(ia, ib, ic, d)
+    | Cmax * BS(ib, ic, d)
+    | rxn Signal(unki, ia, ib, ic) + LS(ia, ib, ic, d) <->{qs}{qmax} HS(unki, ia, ib, ic, d) + BS(ib, ic, d)
+)
+
+(* Degradation reactions *)
+def A_e_0(qi, CiA, Cmax, unki, ia, ib, ic) = new unko new oa (
+      CiA  * Signal(unki, ia, ib, ic)
+    | Cmax * G_1(ia, ib, ic, unko, oa)
+    | rxn Signal(unki, ia, ib, ic) + G_1(ia, ib, ic, unko, oa) ->{qi} Waste1_uni(unki, ia, ib, ic) + P_1(ib, ic, unko, oa)
+)
+
+def ApB_e_0(
+    qi, qmax, CiA, CiB, Cmax,
+    unka, i1a, i1b, i1c,
+    unkb, i2a, i2b, i2c
+) = new unko new oa (
+      CiA  * Signal(unka, i1a, i1b, i1c)
+    | CiB  * Signal(unkb, i2a, i2b, i2c)
+    | Cmax * L_1(i1a, i1b, i1c, i2a, i2b, i2c, unko, oa)
+    | Cmax * B(i1b, i1c, i2a)
+    | rxn Signal(unka, i1a, i1b, i1c) + L_1(i1a, i1b, i1c, i2a, i2b, i2c, unko, oa) <->{qi}{qmax} H_1(unka, i1a, i1b, i1c, i2a, i2b, i2c, unko, oa) + B(i1b, i1c, i2a)
+    | rxn Signal(unkb, i2a, i2b, i2c) + H_1(unka, i1a, i1b, i1c, i2a, i2b, i2c, unko, oa) ->{qmax} Waste1_bi(unka, unkb, i1a, i1b, i1c, i2a, i2b, i2c) + P_1(ib, ic, unko, oa)
+)'
+
+    return(s)
+}
+
+# TODO: Doc this function
+get_dsd_AeB_str <- function(
+    qi, qmax, CiA, CiB, Cmax,
+    unki, ia, ib, ic,
+    unko, oa, ob, oc
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_AeBpC_str <- function(
+    qi, qmax, CiA, CiB, CiC, Cmax,
+    unki, ia, ib, ic,
+    unko1, o1a, o1b, o1c,
+    unko2, o2a, o2b, o2c
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_ApBeC_str <- function(
+    qi, qmax, CiA, CiB, CiC, Cmax,
+    unka, i1a, i1b, i1c,
+    unkb, i2a, i2b, i2c,
+    unkc, c1, c2, c3
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_ApBeCpD_str <- function(
+    qi, qmax, CiA, CiB, CiC, CiD, Cmax,
+    unka, i1a, i1b, i1c,
+    unkb, i2a, i2b, i2c,
+    unkc, o1a, o1b, o1c,
+    unkd, o2a, o2b, o2c
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_buff_str <- function(
+    qs, qmax, Cmax, Cii, d,
+    unki, ia, ib, ic
+) {
+
+}
+
+# TODO: Doc this function
+get_dsd_Ae0_str <- function(
+    qi, CiA, Cmax, unki, ia, ib, ic
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_ApBe0_str <- function(
+
+) {
+    # TODO: Implement this function
+}
+
+# TODO: Doc this function
+get_dsd_def_str <- function(key, val) {
+    # String template
+    s <- 'def %k = %v'
+
+    # Set the key and value
+    s <- stringr::str_replace(s, '%k', key)
+    s <- stringr::str_replace(s, '%v', as.character(val))
+
+    return(s)
+}
+
+# TODO: Doc this function
+save_dsd_script <- function(
+    species,
+    ci,
+    reactions,
+    ki,
+    qmax,
+    cmax,
+    alpha,
+    beta,
+    t,
+    filename
+) {
+    # Check if all reactions attend 4-domain requirements
+    for(r in reactions) {
+        if(!check_reaction_4domain(r)) {
+            stop(paste('Failed to process reaction', r))
+        }
+    }
+
+    # Initialize new reaction configs
+    new_cis <- c(ci * beta)
+    new_kis <- c()
+
+    # Get lambda value and buffer k
+    buffer_stuff <- get_buff_modules(reactions, ki, qmax, cmax)
+
+    # Change cis according to the lambda^{-1} factor
+    if(!is.null(buffer_stuff)) {
+        for(i in 1:length(new_cis)) {
+            new_cis[i] <- new_cis[[i]] * buffer_stuff$lambda_1
+        }
+    }
+
+    # Set the script header
+    script_str <- paste(get_dsd_header_str(t, species), '\n\n', sep = '')
+
+    # Add 4-domain modules
+    script_str <- paste(
+        script_str, get_dsd_4domain_modules_str(), '\n\n', sep= ''
+    )
+
+    # Set Cmax and qmax
+    script_str <- paste(script_str, get_dsd_def_str('Cmax', cmax), sep = '\n')
+    script_str <- paste(script_str, get_dsd_def_str('qmax', qmax), sep = '\n')
+
+    # Set the initial concentration of the species
+    for(i in 1:length(species)) {
+        key <- paste('Ci', species[[i]], sep = '')
+        script_str <- paste(
+            script_str, get_dsd_def_str(key, new_cis[[i]]), sep = '\n'
+        )
+    }
+
+    # Set the rate constants
+    script_str <- paste(script_str, '\n', sep = '')
+    for(i in 1:length(reactions)) {
+        if(is_bimolecular(reactions[i])) {
+            # Recalculate ki according to the buffer module theory
+            k <- ki[i] / (alpha * beta)
+            if(!is.null(buffer_stuff)) {
+                k <- k * buffer_stuff$lambda_1
+            }
+            new_kis <- c(new_kis, k)
+
+            # Add k to the script
+            script_str <- paste(
+                script_str, get_dsd_def_str(
+                    paste('k', as.character(i), sep = ''), k
+                ), sep = '\n'
+            )
+        } else {
+            # Recalculate ki according to the buffer module theory
+            k <- ki[i] / alpha / cmax
+            if(!is.null(buffer_stuff)) {
+                k <- k * buffer_stuff$lambda_1
+            }
+            new_kis <- c(new_kis, k)
+
+            # Add k to the script
+            script_str <- paste(
+                script_str, get_dsd_def_str(
+                    paste('k', as.character(i), sep = ''), k
+                ), sep = '\n'
+            )
+        }
+    }
+
+    # Set buffer rate constants
+    bff_kis <- c()
+    # If there is buffer modules to add
+    if(!is.null(buffer_stuff)) {
+        # Set a counter for naming purposes
+        count <- 1
+
+        # Iterate over the rate constant values that aren't cmax
+        # (all the even indexes are cmax rate constants)
+        for(i in seq(1, length(buffer_stuff$new_ks), by = 2)) {
+            # Get the k for the buffer module
+            bff_kis <- c(bff_kis, buffer_stuff$new_ks[[i]])
+
+            # Add the k to the script (with the name kb)
+            script_str <- paste(
+                script_str, get_dsd_def_str(
+                    paste('kb', as.character(count), sep = ''),
+                    buffer_stuff$new_ks[[i]]
+                ), sep = '\n'
+            )
+
+            count <- count + 1
+        }
+    }
+
+    # Set species with their domains
+    script_str <- paste(script_str, '\n', sep = '')
+    domain_counter <- 1
+    species_domains <- list()
+    for(spec in species) {
+        # Each species has 4 domains
+        species_domains[spec] <- c(
+            paste('d', as.character(domain_counter)),
+            paste('d', as.character(domain_counter + 1)),
+            paste('d', as.character(domain_counter + 2)),
+            paste('d', as.character(domain_counter + 3))
+        )
+
+        # Set the species definition to the script
+        script_str <- paste(script_str, get_dsd_species_str(
+                spec, species_domains[spec]
+            ), sep = '\n'
+        )
+
+        # Add the offset to the counter
+        domain_counter <- domain_counter + 4
+    }
+
+    # TODO: Add reactions to the script
+    script_str <- paste(script_str, '\n', sep = '')
+}
